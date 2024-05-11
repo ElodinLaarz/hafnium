cadbdcec8c636e3533a8c24f91074923270ad680
Agentwolf187 <jontim45@gmail.com>
2024-05-11 12:57:58 -0800

    Change code
    changed code

--- Hafnium/scripts/character_movement.gd
+++ Hafnium/scripts/character_movement.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 400 # Speed in pixels per second.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since previous frame.
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
