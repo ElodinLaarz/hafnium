54c017f8a1e60f12e0a4b5c47119e1e7c15c7f4f
Caleb Geiger <caleblgeiger@gmail.com>
2024-05-11 17:35:00 -0400

    Update character_movement.gd
    Lerp velocity and allow multi-keys to be pressed to adjust velocity.
    
    Also realized positive y is "down", which I will surely forget more than once.

--- Hafnium/scripts/character_movement.gd
+++ Hafnium/scripts/character_movement.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
+# (ElodinLaarz): Values randomly chosen-- feel free to play around for something more fun. 
 const SPEED = 400 # Speed in pixels per second.
+const ACCEL = 10.0
+
+# Crate unit vector in direction determined by currently pressed keys.
+func unit_direction() -> Vector2:
+	var input: Vector2
+	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input.y = Input.get_action_strength("down") - Input.get_action_strength("up") # Yo, down is up?
+	return input.normalized() # Do we need to handle 0 vector?
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
+	var playerDirection: Vector2 = unit_direction() # Could pass along the body here, if ambiguous.
+	
+	# Smooth veolcity between current and desired velocity.
+	velocity = lerp(velocity, playerDirection * SPEED, delta * ACCEL)
 	
 	move_and_slide()
-	pass
