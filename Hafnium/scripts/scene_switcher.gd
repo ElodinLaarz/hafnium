ebefcdd92fd116072b1a7a3bf23a97d639d34e94
Caleb Geiger <caleblgeiger@gmail.com>
2024-05-19 17:50:33 -0400

    add menus to canvas layer

--- Hafnium/scripts/scene_switcher.gd
+++ Hafnium/scripts/scene_switcher.gd
@@ -6,7 +6,7 @@ var curret_game_type: Common.GameType = Common.GameType.SINGLE_PLAYER
 
 func handle_game_start(main_menu: Control, game_type: Common.GameType):
 	var level_to_load: Node2D = load("res://scenes/main_scene.tscn").instantiate()
-	var multiplayer_overlay: Control = level_to_load.get_node(MULTIPLAYER_OVERLAY)
+	var multiplayer_overlay: Control = level_to_load.get_node("UI").get_node(MULTIPLAYER_OVERLAY)
 	match game_type:
 		Common.GameType.SINGLE_PLAYER:
 			multiplayer_overlay.visible = false
