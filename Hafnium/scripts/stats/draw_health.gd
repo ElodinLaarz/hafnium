823bb5174cc07b1c17191366e05c15cc996379b7
Caleb Geiger <caleblgeiger@gmail.com>
2024-06-09 07:16:22 -0400

    update draw health

--- Hafnium/scripts/stats/draw_health.gd
+++ Hafnium/scripts/stats/draw_health.gd
@@ -2,11 +2,18 @@ extends Node
 
 const PLAYER_HEART = preload("res://scenes/interface/lifebar/heart.tscn")
 
+func create_child()
+
+func create_heart_child():
+	var heart = PLAYER_HEART.instance()
+	add_child(heart)
+	return heart
+
 func update_health(player_character: PlayerCharacter):
 	var stats: Stats = player_character.player_stats
 	var pc: ClassHandler.PlayerClass = player_character.player_class
-	# Make sure you have the right number of child components with hearts.
+	# Make sure you have the right number of child components with 
+	# hearts.
 	# Update the icon for each heart based on the current status.
 	# TODO(ElodinLaarz): Could probably ask each class for a map
 	# to draw their health...
-	
