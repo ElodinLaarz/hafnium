2bae84546b85007f4ef2c422dc1c4f6e4827dcd3
Caleb Geiger <caleblgeiger@gmail.com>
2024-06-16 19:43:37 -0400

    remove improper signal parameter

--- Hafnium/scripts/main_menu.gd
+++ Hafnium/scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_single_player_pressed(save_file: String = ""):
         print("save file is empty, defaulting to wizard")
         save_file = "wizard"
     print("singleplayer enabled!")
-    Common.start_game_type.emit(Common.START_GAME_TYPE, self, Common.GameType.SINGLE_PLAYER, save_file)
+    Common.start_game_type.emit(self, Common.GameType.SINGLE_PLAYER, save_file)
 
 func _on_load_pressed():
     print("load enabled!")
@@ -20,12 +20,12 @@ func _on_quit_pressed():
 
 func _on_wizard_pressed():
     print("wizard enabled!")
-    Common.start_game_type.emit(Common.START_GAME_TYPE, self, Common.GameType.SINGLE_PLAYER, "wizard")
+    Common.start_game_type.emit(self, Common.GameType.SINGLE_PLAYER, "wizard")
 
 func _on_druid_pressed():
     print("druid enabled!")
-    Common.start_game_type.emit(Common.START_GAME_TYPE, self, Common.GameType.SINGLE_PLAYER, "druid")
+    Common.start_game_type.emit(self, Common.GameType.SINGLE_PLAYER, "druid")
 
 func _on_barbarian_pressed():
     print("barbarian enabled!")
-    Common.start_game_type.emit(Common.START_GAME_TYPE, self, Common.GameType.SINGLE_PLAYER, "barbarian")
+    Common.start_game_type.emit(self, Common.GameType.SINGLE_PLAYER, "barbarian")
