c1da0203b3ab0936a93526f53829e596f246e6cf
Caleb Geiger <caleblgeiger@gmail.com>
2024-06-30 16:49:15 -0400

    add enemy spawner to level 1

--- Hafnium/scripts/enemy_spawner.gd
+++ Hafnium/scripts/enemy_spawner.gd
@@ -1,5 +1,19 @@
 extends Node2D
+class_name enemy_spawner
+
+var enemy_resource: Resource = load("res://scenes/npcs/slime.tscn")
+
+var last_spawn_time: float = 0.0
+var spawn_rate: float = 1.0
+var spawn_radius: float = 100.0
 
 func spawn(e: PackedScene) -> void:
-	var enemy = e.instance()
-	add_child(enemy)
+    var spawned_enemy = e.instantiate()
+    spawned_enemy.position = position + Common.a_little_offset(spawn_radius)
+    get_parent().add_child(spawned_enemy)
+
+func _process(delta):
+    last_spawn_time += delta
+    if last_spawn_time > spawn_rate:
+        spawn(enemy_resource)
+        last_spawn_time = 0.0
